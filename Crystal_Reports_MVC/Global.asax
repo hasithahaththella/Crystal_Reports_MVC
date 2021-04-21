845619adaae2dae0a558687d4f476b3f9e60d928
Hasitha <HasithaHaththella@gmail.com>
2021-04-22 10:29:58 +1200

    Add project files.

---
+++ Crystal_Reports_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Crystal_Reports_MVC.MvcApplication" Language="C#" %>
---
+++ Crystal_Reports_MVC/Reports/Report.aspx.cs
@@ -0,0 +1,24 @@
+﻿using Crystal_Reports_MVC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Crystal_Reports_MVC.Reports
+{
+    public partial class Report : System.Web.UI.Page
+    {
+protected void Page_Load(object sender, EventArgs e)
+{
+    CrystalReportViewer1.ToolPanelView = CrystalDecisions.Web.ToolPanelViewType.None;
+    CustomerReport crystalReport = new CustomerReport();
+    NorthwindEntities entities = new NorthwindEntities();
+    crystalReport.SetDataSource(from customer in entities.Customers.Take(10)
+                                    select customer);
+    CrystalReportViewer1.ReportSource = crystalReport;
+    CrystalReportViewer1.RefreshReport();
+}
+    }
+}
